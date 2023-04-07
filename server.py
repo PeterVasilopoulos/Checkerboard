81da171a28e1f0b018aa7100c2b1bff912f85598
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-07 10:11:11 -0400

    Assignment completed

--- server.py
+++ server.py
@@ -9,5 +9,17 @@ def default():
 def rows_only(rows):
     return render_template("checkerboard.html", cols = 8, rows = rows)
 
+@app.route('/<int:rows>/<int:cols>')
+def rows_cols(rows, cols):
+    return render_template("checkerboard.html", cols = cols, rows = rows)
+
+@app.route('/<int:rows>/<int:cols>/<color1>')
+def rows_cols_colors1(rows, cols, color1):
+    return render_template("checkerboard.html", cols = cols, rows = rows, color1 = color1)
+
+@app.route('/<int:rows>/<int:cols>/<color1>/<color2>')
+def rows_cols_colors2(rows, cols, color1, color2):
+    return render_template("checkerboard.html", cols = cols, rows = rows, color1 = color1, color2 = color2)
+
 if __name__ == "__main__":
     app.run(debug = True)
