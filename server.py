8081cd330c2371a1b32c6c0214f27ccb22b87152
PeterVasilopoulos <peterjvasilopoulos@gmail.com>
2023-04-07 10:03:25 -0400

    Board is working

--- server.py
+++ server.py
@@ -3,7 +3,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def default():
-    return render_template("checkerboard.html")
+    return render_template("checkerboard.html", cols = 8, rows = 8)
+
+@app.route('/<int:rows>')
+def rows_only(rows):
+    return render_template("checkerboard.html", cols = 8, rows = rows)
 
 if __name__ == "__main__":
     app.run(debug = True)
